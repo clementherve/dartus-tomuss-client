572315edf792aebfc557a7d621bb0951542c906b
Clément HERVE <clement.herve69@gmail.com>
2022-01-25 09:20:32 +0100

    Fix in Constants

--- lib/src/constant/constants.dart
+++ lib/src/constant/constants.dart
@@ -1,6 +1,6 @@
 class Constants {
   static const String _base = "univ-lyon1.fr";
-  static const String tomuss = "https://tomuss.$_base.fr";
+  static const String tomuss = "https://tomuss.$_base";
   static const String caslogin = "https://cas.$_base/cas/login";
   static const String userAgent =
       "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0";
