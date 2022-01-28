75d2e75508fe87df54d17f0516230165204d3202
clementherve <clement.herve69@gmail.com>
2022-01-28 22:58:25 +0100

    exposed the authentication API for the other packages

--- lib/tomuss.dart
+++ lib/tomuss.dart
@@ -1,4 +1,6 @@
 export 'package:dartus/src/dartus.dart' show Dartus;
+export 'package:dartus/src/authentication/authentication.dart'
+    show Authentication;
 export 'package:dartus/src/model/grade.dart' show Grade;
 export 'package:dartus/src/model/teachingunit.dart' show TeachingUnit;
 export 'package:dartus/src/model/teacher.dart' show Teacher;
